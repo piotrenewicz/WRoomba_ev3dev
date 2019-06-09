29918a9d1ba1a69bff3ee8db02f2fee9fdb93ffb
Piotrenewicz <piotrekm9@onet.eu>
2019-06-09 23:46:32 +0200

    Testing sensor access.

--- controls/Read_Distance.py
+++ controls/Read_Distance.py
@@ -2,9 +2,5 @@ import ev3dev.ev3 as ev3
 
 
 def read_distance():
-    while True:
-        sensor = ev3.UltrasonicSensor()
-        print(sensor.value())
-        
-
-read_distance()
+    sensor = ev3.UltrasonicSensor()
+    return sensor.value()
