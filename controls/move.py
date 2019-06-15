2aa282afdd697e2476a0a7fe77763f5d27c19ef6
Piotrenewicz <piotrekm9@onet.eu>
2019-06-15 16:29:29 +0200

    Testing motor access.

--- controls/move.py
+++ controls/move.py
@@ -3,7 +3,9 @@ import ev3dev.ev3 as ev3
 
 def move_head(pos):
     motor = ev3.MediumMotor()
-    motor.position_sp = pos
-    motor.run_to_abs_pos()
+    print(motor.position)
+    # 
+    # motor.position_sp = pos
+    # motor.run_to_abs_pos()
 
 move_head(100)
